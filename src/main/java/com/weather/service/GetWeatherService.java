cd4b94c9859da762285a15e0a437b44f1198e6ba
Mikhail <mihantoff@yandex.ru>
2021-09-07 09:47:42 -0900

    Initial commit

---
+++ src/main/java/com/weather/service/GetWeatherService.java
@@ -0,0 +1,7 @@
+package com.weather.service;
+
+public class GetWeatherService {
+    public double getCurrentTempreture(){
+        return 0.0;
+    }
+        }
---
+++ src/main/java/com/weather/service/SendMessageService.java
@@ -0,0 +1,8 @@
+package com.weather.service;
+
+public class SendMessageService {
+
+    public void sendMessage (String message){
+
+    }
+}
