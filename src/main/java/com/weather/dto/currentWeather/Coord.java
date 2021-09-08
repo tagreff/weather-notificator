666c6dd1469519e7b5860903886901adee2a54a8
Mikhail <mihantoff@yandex.ru>
2021-09-08 12:26:14 -0900

    return Entity CurrentWeather

--- src/main/java/com/weather/dto/currentWeather/Coord.java
+++ src/main/java/com/weather/dto/currentWeather/Coord.java
@@ -2,8 +2,32 @@ package com.weather.dto.currentWeather;
 
 import lombok.Data;
 
-@Data
+
 public class Coord {
-    private Double ion;
-    private Double iat;
+    private double lon;
+    private double lat;
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    @Override
+    public String toString() {
+        return "Coord{" +
+                "lon=" + lon +
+                ", lat=" + lat +
+                '}';
+    }
 }
--- src/main/java/com/weather/service/GetWeatherService.java
+++ src/main/java/com/weather/service/GetWeatherService.java
@@ -1,15 +1,25 @@
 package com.weather.service;
 
+import com.weather.dto.currentWeather.CurrentWeather;
 import com.weather.exception.WeatherAPIException;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import static org.springframework.http.HttpMethod.*;
+
+@Component("GetWeatherService")
 public class GetWeatherService {
-    public double getCurrentTemperature() {
+    public CurrentWeather getCurrentTemperature() {
         final String url = "https://api.openweathermap.org/data/2.5/weather?id=523750&appid=740bff51e977096cbba34ebfa8bf3644";
         RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(url, String.class);
-
-        return parsingTemperature(result);
+        ResponseEntity<CurrentWeather> result = restTemplate.exchange(url, GET, null, new ParameterizedTypeReference<CurrentWeather>() {
+        });
+        CurrentWeather currentWeather = result.getBody();
+        return currentWeather;
+//        return parsingTemperature(result);
     }
 
     private double parsingTemperature(String json) {
