e692d5a19e0b6b4bb7f6c39eab06f1dadd44624c
tagreff <tagreff@yandex.ru>
2021-09-10 18:53:20 +0300

    added number validator false test

--- src/test/java/com/weather/utils/PhoneNumberValidatorTest.java
+++ src/test/java/com/weather/utils/PhoneNumberValidatorTest.java
@@ -2,12 +2,18 @@ package com.weather.utils;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PhoneNumberValidatorTest {
     final String validNumber = "9111471546";
+    final String invalidNumber = "5555";
     @Test
     public void validateValidNumber_returnsTrue(){
         assertTrue(PhoneNumberValidator.isValid(validNumber));
+    }
+    @Test
+    public void validateInvalidNumber_returnsFalse(){
+        assertFalse(PhoneNumberValidator.isValid(invalidNumber));
     }
 }
